ce642277b8e6a2336b76df96df45cac62bfb3454
schips <schips@dingtalk.com>
2025-04-27 14:51:43 +0800

    libcap : 修复、优化脚本
    1. 修复编译脚本没有正确指定编译工具链的问题
    2. 对外提供包链接
    3. 支持编译host版本

--- .common_lib/libcap.sh
+++ .common_lib/libcap.sh
@@ -1,27 +1,65 @@
-
 export LIBCAP=libcap
 
-export LIBCAP_VERSION=2.67  # 03-Feb-2023 05:20
+export CONFIG_LIBCAP_VERSION=2.67 # 03-Feb-2023 05:20
+export LIBCAP_VERSION=${LIBCAP}-$CONFIG_LIBCAP_VERSION  # 03-Feb-2023 05:20
 
 export LIBCAP_OUTPUT_PATH=${OUTPUT_PATH}/${LIBCAP}
+export LIBCAP_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/${LIBCAP}
 
-function download_libcap () {
+## for others
+export LIBCAP_FILE_NAME=${LIBCAP_VERSION}.tar.gz
+export LIBCAP_ARCH_PATH=$ROOT_DIR/libcap/compressed/${LIBCAP_FILE_NAME}
+
+function _sync_export_var_vsftpd()
+{
+    export LIBCAP_VERSION=${LIBCAP}-$CONFIG_LIBCAP_VERSION
+    export LIBCAP_FILE_NAME=${LIBCAP_VERSION}.tar.gz
+    export LIBCAP_ARCH_PATH=$ROOT_DIR/libcap/compressed/${LIBCAP_FILE_NAME}
+}
+
+function get_libcap () {
+    _sync_export_var_vsftpd
     # https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/
-    tget   https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-${LIBCAP_VERSION}.tar.gz
+    tget_package_from_arch  $LIBCAP_ARCH_PATH $ARCHIVE_PATH/$LIBCAP_FILE_NAME  https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/${LIBCAP_VERSION}.tar.gz
 }
 
 function mk_libcap () {
-    libcap_dir=${CODE_PATH}/libcap*${1}*
+    libcap_dir=${CODE_PATH}/${LIBCAP_VERSION}
     cd $libcap_dir;
+    cat <<EOF > $tmp_config
     export GOLANG=no # 让libcap不通过go来编译，采取常规编译比较快
     make clean -C $libcap_dir
-    CROSS_COMPILE=aarch64-none-linux-gnu- BUILD_CC=gcc make prefix=${LIBCAP_OUTPUT_PATH} install  -C $libcap_dir
+    CROSS_COMPILE=${BUILD_HOST_} BUILD_CC=gcc make prefix=${LIBCAP_OUTPUT_PATH} install  -C $libcap_dir
+EOF
+    bash $tmp_config
+}
+
+function mk_libcap_host () {
+    libcap_dir=${CODE_PATH}/${LIBCAP_VERSION}
+    cd $libcap_dir;
+    cat <<EOF > $tmp_config
+    export GOLANG=no # 让libcap不通过go来编译，采取常规编译比较快
+    make clean -C $libcap_dir
+    CROSS_COMPILE="" BUILD_CC=gcc make prefix=${LIBCAP_OUTPUT_PATH_HOST} install  -C $libcap_dir
+EOF
+    bash $tmp_config
 }
 
 function make_libcap ()
 {
-    download_libcap  || return 1
+    _sync_export_var_vsftpd
+    get_libcap  || return 1
     tar_package || return 1
 
-    mk_libcap $LIBCAP_VERSION
+    mk_libcap
 }
+
+function make_libcap_host ()
+{
+    _sync_export_var_vsftpd
+    get_libcap  || return 1
+    tar_package || return 1
+
+    mk_libcap_host
+}
+
--- libcap/make.sh
+++ libcap/make.sh
@@ -3,6 +3,6 @@ source ../.common
 #export LIBCAP_VERSION=2.67  # 03-Feb-2023 05:20
 
 #export LIBCAP_OUTPUT_PATH=${OUTPUT_PATH}/${LIBCAP}
-
+#export LIBCAP_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/${LIBCAP}
 
 make_libcap
