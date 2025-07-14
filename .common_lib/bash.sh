708d0f3137765427487f153f78e4bae22c76136a
schips <schips@dingtalk.com>
2025-07-14 11:51:30 +0800

    bash : 支持编译host版本

--- .common_lib/bash.sh
+++ .common_lib/bash.sh
@@ -3,23 +3,46 @@ export BASH=bash
 export CONFIG_BASH_VERSION=4.3
 export BASH_VERSION=${BASH}-${CONFIG_BASH_VERSION}
 export BASH_OUTPUT_PATH=${OUTPUT_PATH}/bash
+export BASH_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/bash
 
 function download_bash () {
+    _sync_export_var_bash
     #tget    https://ftp.gnu.org/gnu/bash/bash-5.1.8.tar.gz
     tget    https://ftp.gnu.org/gnu/bash/bash-${CONFIG_BASH_VERSION}.tar.gz
 }
 
+function _sync_export_var_bash()
+{
+    export BASH_VERSION=${BASH}-${CONFIG_BASH_VERSION}
+}
+
 function mk_bash () {
+
+    local build_for_host="$1" # say y for host
+
+    _sync_export_var_bash
+    local build_with_target_arch_cmd_part=""
+    local cache_file_path_cmd_part=""
+    local output_dir=""
+    if [  "$build_for_host" == 'y' ];then
+        cache_file_path_cmd_part="--cache-file=host.cache"
+        build_with_target_arch_cmd_part=""
+        output_dir=${BASH_OUTPUT_PATH_HOST}
+    else
+        cache_file_path_cmd_part="--cache-file=arm-linux.cache"
+        build_with_target_arch_cmd_part="--host=arm-linux  CC=\"${_CC}\""
+        output_dir=${BASH_OUTPUT_PATH}
+    fi
+
     bash <<EOF
 
     cd ${CODE_PATH}/$BASH_VERSION
 
-    ./configure CC=${_CC} --prefix=${BASH_OUTPUT_PATH} \
-        --host=arm-linux \
-        --target=${BUILD_HOST} \
+    ./configure ${build_with_target_arch_cmd_part} \
+        --prefix=${output_dir} \
         --enable-history \
         --without-bash-malloc  \
-        --cache-file=arm-linux.cache
+        ${cache_file_path_cmd_part}
     make clean
     make $MKTHD && make install
 EOF
@@ -43,11 +66,21 @@ EOF
 
 function make_bash ()
 {
-    export BASH_VERSION=${BASH}-${CONFIG_BASH_VERSION}
+    _sync_export_var_bash
     download_bash  || return 1
     tar_package || return 1
 
     mk_bash  || return 1
+    echo_bash_help
+}
+
+function make_bash_host ()
+{
+    _sync_export_var_bash
+    download_bash  || return 1
+    tar_package || return 1
+
+    mk_bash y || return 1
     echo_bash_help
 }
 
--- bash/make.sh
+++ bash/make.sh
@@ -3,5 +3,7 @@ source ../.common
 
 #export CONFIG_BASH_VERSION=4.3
 #export BASH_OUTPUT_PATH=${OUTPUT_PATH}/bash
+#export BASH_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/bash
 
 make_bash
+#make_bash_host
