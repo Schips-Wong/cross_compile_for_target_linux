769cd344746ddb6a23d4281845a643c7e13af12c
schips <schips@dingtalk.com>
2026-03-17 14:39:15 +0800

    live555 : 支持host编译

--- .common_lib/live555.sh
+++ .common_lib/live555.sh
@@ -6,6 +6,7 @@ export CONFIG_LIVE555_VERSION=latest
 export LIVE555_VERSION=${LIVE555}-${CONFIG_LIVE555_VERSION}
 
 export LIVE555_OUTPUT_PATH=${OUTPUT_PATH}/${LIVE555}
+export LIVE555_OUTPUT_PATH_HOST=${OUTPUT_PATH_HOST}/${LIVE555}
 
 function get_live555 () {
     if [ "$CONFIG_LIVE555_VERSION" = "latest" ]; then
@@ -16,31 +17,50 @@ function get_live555 () {
 }
 
 function mk_live555 () {
+	local build_for="$1"
     cd ${CODE_PATH}/live || return 1
+
+    local output_dir=""
+    local openssl_dir=""
+    local mk_file_name=""
+    local using_cross_compile_arg=""
+
+    if [ "$build_for" != "host" ];then
+        output_dir=$LIVE555_OUTPUT_PATH
+        openssl_dir=${OPENSSL_OUTPUT_PATH}
+        mk_file_name=armlinux
+        using_cross_compile_arg="CROSS_COMPILE=${BUILD_HOST_}"
+    else
+        output_dir=$LIVE555_OUTPUT_PATH_HOST
+        openssl_dir=${OPENSSL_OUTPUT_PATH_HOST}
+        mk_file_name=linux-64bit
+        using_cross_compile_arg=""
+    fi
 
 read -r -d '' NEW_COPTS <<- EOF
 \$(INCLUDES)   \
 -I. \
 -IliveMedia/include/ \
--I${OPENSSL_OUTPUT_PATH}/include \
--L${OPENSSL_OUTPUT_PATH}/lib \
+-I${openssl_dir}/include \
+-L${openssl_dir}/lib \
 -O2 -DSOCKLEN_T=socklen_t -DNO_SSTREAM=1 -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64 \
 -DNO_STD_LIB -lssl -lcrypto
 EOF
 
-    ./genMakefiles  armlinux || return 1
+    ./genMakefiles  $mk_file_name || return 1
 
-    export NEW_LIBS_CONSOLE="-I${OPENSSL_OUTPUT_PATH}/include -L${OPENSSL_OUTPUT_PATH}/lib -lssl -lcrypto"
+    export NEW_LIBS_CONSOLE="-I${openssl_dir}/include -L${openssl_dir}/lib -lssl -lcrypto"
     #bash << EOF #BUG
     export COMPILE_OPTS="${NEW_COPTS}"
     export LIBS_FOR_CONSOLE_APPLICATION="${NEW_LIBS_CONSOLE}"
     make clean
-    make CROSS_COMPILE=${BUILD_HOST_} \
+    make ${using_cross_compile_arg} \
         COMPILE_OPTS="${NEW_COPTS}" \
         LIBS_FOR_CONSOLE_APPLICATION="${NEW_LIBS_CONSOLE}" \
-        $MKTHD && make install PREFIX=${LIVE555_OUTPUT_PATH}
+        $MKTHD && make install PREFIX=${output_dir}
     #EOF
 }
+
 
 function make_live555 () {
     rm ${CODE_PATH}/live -rf
@@ -48,7 +68,18 @@ function make_live555 () {
     get_ssl
 
     tar_package       || return 1
-    make_ssl
+    make_openssl
     mk_live555
+}
+
+
+function make_live555_host () {
+    rm ${CODE_PATH}/live -rf
+    get_live555
+    get_ssl
+
+    tar_package       || return 1
+    make_openssl_host
+    mk_live555 "host"
 }
 
